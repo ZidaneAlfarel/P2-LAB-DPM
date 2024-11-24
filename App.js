d322d56482038cb302db34abaebd96b5009a1d72
ZidaneAlfarel <168409445+ZidaneAlfarel@users.noreply.github.com>
2024-11-24 15:00:59 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Teks di tengah layar */}
+      <Text style={styles.text}>Hello World</Text>
+
+      {/* Dua kotak dengan FlexBox */}
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#d3d3d3", // Abu-abu terang
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: "blue",
+    fontWeight: "bold",
+    marginBottom: 50, // Jarak dari kotak
+  },
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%", // Lebar total area kotak
+  },
+  box1: {
+    backgroundColor: "purple", // Warna baru
+    width: 100,
+    height: 100,
+  },
+  box2: {
+    backgroundColor: "orange", // Warna baru
+    width: 100,
+    height: 100,
+  },
+});
